e3c7a42b371ecf5b13754fc0ba24e595d24cc6b1
Madhavi <Madhavi@example.com>
2022-09-19 10:14:20 +0100

    updated integration testcase

---
+++ src/main/resources/books-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (author, isbn, name, pages, price) VALUES ('Robert Kiosaki-1', '97810011', 'Rich Dad poor Dad-1', '255', '15');
+INSERT INTO books (author, isbn, name, pages, price) VALUES ('Robert Kiosaki-2', '97810012', 'Rich Dad poor Dad-2', '256', '15');
+INSERT INTO books (author, isbn, name, pages, price) VALUES ('Robert Kiosaki-3', '97810013', 'Rich Dad poor Dad-3', '257', '15');
